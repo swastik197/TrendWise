4fdaf8539939569026c1e7aef83bf58ddb03c10b
Swastik Sahu <sahuswastik50@gmail.com>
2025-07-03 17:11:29 +0530

    Update gemini.js 1.5 to 2.0

--- trenwise_backend/bot_controller/gemini.js
+++ trenwise_backend/bot_controller/gemini.js
@@ -56,7 +56,7 @@ const slugify = require('slugify');
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+const model = genAI.getGenerativeModel({ model: 'gemini-2.0-flash' });
 
 async function generate(input) {
   try {
